4a0dea0ca3ff365ddeb33bec6dd72a09e22734af
osman-developer <57558743+osman-developer@users.noreply.github.com>
2021-03-11 10:23:19 +0200

    custom

--- src/main/java/io/javaapi/course/course/CourseRepository.java
+++ src/main/java/io/javaapi/course/course/CourseRepository.java
@@ -1,9 +1,16 @@
 package io.javaapi.course.course;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CourseRepository extends CrudRepository<Course,String> {
 	// it is the DALC
 	// a built in crud operations
-
+	//you can add how the method looks like and JPA will implement it, it sounds crazy
+	//since interface so abstract method
+	//custom find method should start with Find
+	
+	//this is a custom method to find courses by name, JPA is smart enough to read it and know what you want
+	public List<Course> findByName(String topicId);
 }
